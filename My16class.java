b61675b8fb9bab7a14a36fdf8d8ace98729fbdba
Tako11177 <takokorinteli7@gmail.com>
2025-05-11 03:02:57 +0400

    Array int

---
+++ My16class.java
@@ -0,0 +1,21 @@
+public class My16class {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+        numbers[0]=10;
+        numbers[1]=15;
+        numbers[2]=30;
+
+        System.out.println(numbers [1]);
+
+
+        int[] score = {10, 15, 30};
+
+        System.out.println(score[2]);
+    }
+}
+
+/* პასუხი:
+15
+30
+ *///
