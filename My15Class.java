1a13b544eb0843cdb78789f27d0fdec8c4e4305b
Tako11177 <takokorinteli7@gmail.com>
2025-05-11 02:41:07 +0400

    Array string

---
+++ My15Class.java
@@ -0,0 +1,16 @@
+public class My15Class {
+    public static void main(String[] args) {
+
+        String [] colors= {"Red", "Green", "Blue", "Orange"};
+        System.out.println(colors[2]);
+
+
+
+    }
+}
+
+/* პასუხი:
+Blue
+ */
+
+
