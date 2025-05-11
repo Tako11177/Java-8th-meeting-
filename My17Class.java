252f88e5dd1b3af23d3cc4faa85b1635c26589bf
Tako11177 <takokorinteli7@gmail.com>
2025-05-11 17:15:37 +0400

    second array

---
+++ My17Class.java
@@ -0,0 +1,30 @@
+public class My17Class {
+    public static void main(String[] args) {
+
+
+        String[] colors = {"Red", "Green", "Blue", "Orange"};
+
+
+        /*for ( int i = 0; i < colors.length; i++){
+
+            System.out.println(colors[i]);*/
+
+
+        int i =0;
+
+        while (i< colors.length) {
+            System.out.println(colors[i]);
+            i++;
+
+
+        }
+    }
+
+    /*პასუხი:
+    Red
+Green
+Blue
+Orange
+25 წთ.
+     */
+}
